60310bac425ac87a0d1c87b813e93499fa2ff2fd
Matthew Gould <mattgould44@gmail.com>
2018-11-03 16:44:58 -0700

    changed loop for solution check need to debug

--- satSolve.cpp
+++ satSolve.cpp
@@ -2,6 +2,7 @@
 #include <fstream> 
 #include <vector> 
 #include <bitset> 
+#include <cmath> 
 
 using namespace std; 
 
@@ -13,7 +14,7 @@ int main(){
     vector<int> solVec;
     string element = " "; 
     ifstream file; 
-    file.open("satTest.CNF");
+    file.open("satTest3.CNF");
     int numVar = 0;
     int numClause = 0; 
     
@@ -22,23 +23,25 @@ int main(){
     while(file >> element){
         if (element.find_first_not_of ("-0123456789") == string::npos){
             clauseVec.push_back(stoi(element));
+            cout << element;
         }
     }
-
+    cout << endl << endl; 
     satTest(clauseVec,solVec); 
-
-    for(int i=0; i<clauseVec[0]; i++){
-        cout << solVec[i] << " "; 
+    cout << endl << endl; 
+    if(solVec.size() != 0){
+        for(int i=0; i<solVec.size(); i++){
+            cout << solVec[i]+1 << " "; 
+        }
     }
-
 
     return 0; 
 }
 
 void satTest(vector<int>& clauseVec,vector<int>& solutionVec){
     int numVar = clauseVec[0];
-    int backTrackNum = 1; 
-    bitset <1024> solutionData (numVar); 
+    int backTrackNum = 1;  
+    bitset <1024> solutionData (pow(2,numVar)-1); 
   
     solutionVec.reserve(numVar); 
 
@@ -48,38 +51,65 @@ void satTest(vector<int>& clauseVec,vector<int>& solutionVec){
            cout << "not satisfiable"; 
            return; 
         }
-        solutionData = bitset<1024> (numVar - backTrackNum); 
+        solutionData = bitset<1024> (pow(2,(numVar - backTrackNum))-1); 
+        cout << "backtrack: " << backTrackNum << endl; 
+        backTrackNum++; 
     }
 
     cout << "numVar: " << numVar << endl; 
     // construct solution vector 
+    cout << solutionData; 
     for(int i=0; i<numVar; i++){
         if(solutionData.test(i)){
-            solutionVec[i] = i; 
+            solutionVec.push_back(i); 
         } else {
-            solutionVec[i] = -i; 
+            solutionVec.push_back(-i); 
         } 
     }
     return; 
 }
 
 bool checkSol(bitset <1024>& solutionData, vector<int>& clauseVec){
+    int numClause = 0; 
+    int index = 2; 
+    cout << solutionData << endl; 
+
     // for each clause check solution data
-     
-    for(int i=1; i<clauseVec.size(); i++){
-        if(clauseVec[i] != 0){
-            if(clauseVec[i] > 0){
-                if(solutionData.test(clauseVec[i])){
+    while(numClause < clauseVec[1]){
+        if(clauseVec[index] > 0){
+            if(solutionData.test(clauseVec[index]-1)){
+                while(clauseVec[index] != 0){
+                 
+                    index++;       
+                }
+                numClause++; 
+                cout << endl << "index " << index << endl; 
+                cout << "clause # " << numClause << endl; 
+                if(numClause == clauseVec[1]){
                     return true; 
-                } 
-            }else{
-                if(solutionData.test(-clauseVec[i])){
-                    return true; 
-                } 
+                }
             }
-
+        } else if (clauseVec[index] < 0){
+            if(solutionData.test(-(clauseVec[index]-1)) == false){
+                while(clauseVec[index] != 0){
+                    index++;       
+                }
+                numClause++; 
+                cout << endl << "index " << index << endl; 
+                cout << "clause # " << numClause << endl; 
+                if(numClause == clauseVec[1]){
+                    return true; 
+                }
+                
+            }
+        } else {
+            numClause++; 
+            if(numClause == clauseVec[1]){
+                return false; 
+            }
         }
+        index++;
     }
-
-    return false; 
+    
+    return true; 
 }
