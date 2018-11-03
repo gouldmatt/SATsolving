1843f5746210bf20e6c43b276ddef119f7fb04d7
Matthew Gould <mattgould44@gmail.com>
2018-11-03 09:29:11 -0700

    reading from file setup

--- satSolve.cpp
+++ satSolve.cpp
@@ -5,19 +5,32 @@
 using namespace std; 
 
 int main(){
-    vector<string> elementVector; 
+    vector<string> clauseVec; 
     string element = " "; 
     ifstream file; 
     file.open("satTest.CNF");
+    int numVar = 0;
+    int numClause = 0; 
+    
 
+    // read entire file into vector keeping only num 
     while(file >> element){
-        if(element.front() != 'c' || element.front() != 'p'){
-            continue; 
+        if (element.find_first_not_of ("-0123456789") == string::npos){
+            clauseVec.push_back(element);
         }
-        elementVector.push_back(element);
-        cout << element << endl; 
-        
     }
+
+    // first element is the num of variables 
+    numVar = stoi(clauseVec[0]);
+    // second is num of clauses 
+    numClause = stoi(clauseVec[1]);
+    //clauseVec.erase(clauseVec.begin(),clauseVec.begin()+2); 
+    
+    for(int i = 0; i < clauseVec.size(); i++){
+        cout << clauseVec[i] << endl; 
+    }
+
+    cout << "clause vec size: " << clauseVec.size() << endl; 
 
 
 
