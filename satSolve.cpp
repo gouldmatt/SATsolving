22ea4c97d90e9033b53f600a8266727b0b08f885
Matthew Gould <mattgould44@gmail.com>
2018-11-03 12:43:53 -0700

    partial implementation for single thread solution

--- satSolve.cpp
+++ satSolve.cpp
@@ -1,11 +1,16 @@
 #include <iostream> 
 #include <fstream> 
 #include <vector> 
+#include <bitset> 
 
 using namespace std; 
 
+vector<int> satTest(vector<string>& clauseVec);
+bool checkSol(bitset <1024>& solutionData, vector<string>& clauseVec);
+
 int main(){
     vector<string> clauseVec; 
+    vector<int> solVec;
     string element = " "; 
     ifstream file; 
     file.open("satTest.CNF");
@@ -20,20 +25,45 @@ int main(){
         }
     }
 
-    // first element is the num of variables 
-    numVar = stoi(clauseVec[0]);
-    // second is num of clauses 
-    numClause = stoi(clauseVec[1]);
-    //clauseVec.erase(clauseVec.begin(),clauseVec.begin()+2); 
-    
-    for(int i = 0; i < clauseVec.size(); i++){
-        cout << clauseVec[i] << endl; 
+    solVec = satTest(clauseVec); 
+
+    for(int i=0; i<solVec.size(); i++){
+        cout << solVec[i] << " "; 
     }
-
-    cout << "clause vec size: " << clauseVec.size() << endl; 
-
-
 
 
     return 0; 
+}
+
+vector<int> satTest(vector<string>& clauseVec){
+    int numVar = stoi(clauseVec[0]);
+    int backTrackNum = 1; 
+    vector<int> solutionVec;
+    bitset <1024> solutionData (numVar); 
+  
+    solutionVec.reserve(numVar); 
+
+    while(checkSol(solutionData,clauseVec) == false){
+        if(backTrackNum > numVar){
+           // equation is not satisfiable 
+           return solutionVec; 
+        }
+        solutionData = bitset<1024> (numVar - backTrackNum); 
+    }
+
+    // construct solution vector 
+    for(int i=0; i<numVar; i++){
+        if(solutionData[i] == 1){
+            solutionVec[i] = i; 
+        } else {
+            solutionVec[i] = -i; 
+        } 
+    }
+    return solutionVec; 
+}
+
+bool checkSol(bitset <1024>& solutionData, vector<string>& clauseVec){
+    // for each clause check solution data 
+
+    return true; 
 }
